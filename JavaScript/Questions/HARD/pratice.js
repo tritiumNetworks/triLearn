508fad504792b614bc536c42e0fbd5431734ffa8
PMHStudio <pmhstudio.pmh@gmail.com>
2020-02-09 16:32:17 +0900

    JS Pratice

---
+++ JavaScript/Questions/HARD/pratice.js
@@ -0,0 +1,9 @@
+/**
+ * [HARD] practice.js
+ * @copyright (c) 2020. Tritium Networks / PMH. MIT Licensed
+ * @license MIT
+ */
+
+let a = Array, b, c = console, d = 'g', e, f = Function, g = 'log'
+b = a(f('c',d,'c[g]`이 메세지가 출력되는 이유를 설명하시오`'))
+b[0](c,g)
